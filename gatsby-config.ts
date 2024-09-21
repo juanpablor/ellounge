146a4c6a236f3a0b5342c291605c4d095a16e0e1
Juan Pablo Riano <jp@pricerqx.com>
2024-09-20 20:22:03 -0400

    working build

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,9 @@
 import type { GatsbyConfig } from "gatsby";
-// const { languages, defaultLanguage } = require('./languages');
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `El Lounge Latin Club`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://pointbarlounge.ca/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -32,18 +31,15 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-react-i18next`,
       options: {
-        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+        localeJsonSourceName: `locale`,
         languages: [`fr`, `es`, `en`],
         defaultLanguage: `fr`,
         siteUrl: `https://example.com`,
-        // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
         trailingSlash: 'always',
-        // you can pass any i18next options
         i18nextOptions: {
           interpolation: {
-            escapeValue: false // not needed for react as it escapes by default
+            escapeValue: false
           },
-          // keySeparator: false,
           nsSeparator: false
         },
         pages: [
@@ -85,7 +81,7 @@ const config: GatsbyConfig = {
           ],
         },
       },
-    },
+    }
   ],
 };
 
